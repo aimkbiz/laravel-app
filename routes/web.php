8381a946b7917c296aa4ca63a6d50e5ae488de20
aimkbiz <aimkbiz@gmail.com>
2024-04-20 16:57:48 +0900

    laravel add

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\GoogleLoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,22 +12,21 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/auth/google', [GoogleLoginController::class, 'redirectToGoogle'])
-    ->name('login.google');
-
-Route::get('/auth/google/callback', [GoogleLoginController::class, 'handleGoogleCallback'])
-    ->name('login.google.callback');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/auth/google', [GoogleLoginController::class, 'redirectToGoogle'])
+    ->name('login.google');
+
+Route::get('/auth/google/callback', [GoogleLoginController::class, 'handleGoogleCallback'])
+    ->name('login.google.callback');
+
 Route::get('/userTodo', 'App\Http\Controllers\UserTodoController@index');
 Route::post('/userTodo', 'App\Http\Controllers\UserTodoController@create');
 Route::put('/userTodo', 'App\Http\Controllers\UserTodoController@update');
 Route::delete('/userTodo', 'App\Http\Controllers\UserTodoController@delete');
-//Route::get('/userTodo', [UserTodoController::class, 'index']);
 
 require __DIR__.'/auth.php';
---
+++ service/config.php
@@ -0,0 +1,5 @@
+    'google' => [
+        'client_id' => env('GOOGLE_KEY'), 
+        'client_secret' => env('GOOGLE_SECRET'), 
+        'redirect' => env('GOOGLE_REDIRECT_URI'), 
+      ],
